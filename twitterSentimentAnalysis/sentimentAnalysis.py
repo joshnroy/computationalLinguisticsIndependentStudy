97457cca637442489d7df58ac605acb156b33f15
Josh Roy <joshnroy@gmail.com>
2015-03-16 18:08:54 -0400

    Planned out how the sentiment engine will work

---
+++ twitterSentimentAnalysis/sentimentAnalysis.py
@@ -0,0 +1,23 @@
+# Learning
+
+## Step 1
+Imports a list of tweets along with their positive and negative connotations
+
+## Step 2
+Perform the following on each tweet
+
+1. Splits the tweet up into bigrams
+2. If the tweet is positive, adds 1 to the sentiment value of the each bigram. If negative, subtracts a 1 from the sentiment value of each bigram
+
+# Application
+
+## Step 1
+Reads input that contains a tweet or some other words
+
+## Step 2
+Adds the values of the words in the text and says it is either positive or negative. (Not sure if I want it to add the values or 1 for the positive and -1 for the negative)
+
+## Step 3
+Asks the user if this is correct
+- If yes, add one to the values of each of the bigrams
+- If no, subtract one from the values of each of the bigrams
