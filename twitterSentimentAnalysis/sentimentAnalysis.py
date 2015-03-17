4fd2341b85460db19e3da536683e58da8265c1b7
Josh Roy <joshnroy@gmail.com>
2015-03-17 16:44:52 -0400

    Made the sentiment analysis engine work with both unigrams and bigrams. Also ran it through some test data

--- twitterSentimentAnalysis/sentimentAnalysis.py
+++ twitterSentimentAnalysis/sentimentAnalysis.py
@@ -1,15 +1,16 @@
 import json
 
 try:
-    with open('Bigrams.json', 'rb') as fp:
-        Bigrams = json.load(fp)
+    with open('sentimentData.json', 'rb') as fp:
+        sentimentData = json.load(fp)
 except ValueError:
-    Bigrams = {}
+    sentimentData = {}
 
 
 
 def getInput():
     return raw_input("Please Enter the text you would like to measure the sentiment of: ") 
+
 def breakIntoBigrams(inputText):
     prev_word = ""
     
@@ -17,32 +18,41 @@ def breakIntoBigrams(inputText):
 
     for word in tokens:
         bigram = prev_word + ' ' + word
-        if not bigram in Bigrams:
-            Bigrams[bigram] = {'bigram': bigram, 'sentiment': 0}
+        if not bigram in sentimentData:
+            sentimentData[bigram] = {'gram': bigram, 'sentiment': 0}
 
         prev_word = word
-    return Bigrams
+    return sentimentData 
+def breakIntoUnigrams(inputText):
+    tokens = inputText.split()
 
-def analyzeSentiment(inputText, sentimentBigrams):
+    for word in tokens:
+        if not word in sentimentData:
+            sentimentData[word] = {'gram': word, 'sentiment': 0}
+    return sentimentData
+
+def analyzeSentiment(inputText):
     overallSentiment = 0
     prev_word = ""
     tokens = inputText.split()
     for word in tokens:
         bigram = prev_word + ' ' + word
-        if bigram in sentimentBigrams:
-            overallSentiment += sentimentBigrams[bigram]['sentiment']
+        if bigram in sentimentData:
+            overallSentiment += sentimentData[bigram]['sentiment']
+        if word in sentimentData:
+            overallSentiment += sentimentData[word]['sentiment']
     print "Sentiment of the entered text was " + str(overallSentiment)
     return overallSentiment
 
 
-def applyFeedback(analysis, Bigrams):
-    feedback = input("Enter 'True' if this analysis was correct (or too low), and 'False' if it wasn't (or was too high): ")
+def applyFeedback(analysis):
+    feedback = input("Enter 'True' if the phrase was positive, and 'False' if the phrase was negative: ")
     if feedback:
-        for bigram in Bigrams:
-            Bigrams[bigram]['sentiment'] += analysis + 1
+        for gram in sentimentData:
+            sentimentData[gram]['sentiment'] += 1
     else:
-        for bigram in Bigrams:
-            Bigrams[bigram]['sentiment'] -= abs(analysis) + 1
+        for gram in sentimentData:
+            sentimentData[gram]['sentiment'] -= 1
 
 
 
@@ -51,18 +61,19 @@ while doAgain:
 # Step 1 - Input and breakup
     text = getInput()
     bigrams = breakIntoBigrams(text)
+    unigrams = breakIntoUnigrams(text)
 
 
 # Step 2 - Analysis
-    analysis = analyzeSentiment(text, bigrams)
+    analysis = analyzeSentiment(text)
 
 
 # Step 3 - Feedback
-    applyFeedback(analysis, bigrams)
+    applyFeedback(analysis)
 
 # Check if user wants to do it again
 #    doAgain = input("Type 'True' if you want to do this again and 'False' if you don't: ")
 
 # Save the bigrams dictionary in json format
-    with open('Bigrams.json', 'wb') as fp:
-        json.dump(Bigrams, fp)
+    with open('sentimentData.json', 'wb') as fp:
+        json.dump(sentimentData, fp)
