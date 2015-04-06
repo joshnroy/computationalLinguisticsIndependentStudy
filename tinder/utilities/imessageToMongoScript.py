e3b27b57d6c5cca26cffe71b33025080d07553cd
Josh Roy <joshnroy@gmail.com>
2015-04-06 10:33:55 -0400

    Changed the mongoscript

--- tinder/utilities/imessageToMongoScript.py
+++ tinder/utilities/imessageToMongoScript.py
@@ -11,7 +11,9 @@ csv_f = csv.reader(f)
 messages = db['messages']
 
 for row in csv_f:
-    message = {"to": row[3],
+    message = {"time": row[0],
+            "to": row[3],
             "from": row[5],
             "message": row[7]}
     message_id = messages.insert(message)
+    print message
